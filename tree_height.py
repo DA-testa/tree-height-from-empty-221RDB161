08e594c0bec40dc930842b371da364ab24f6488f
Linards Beķeris <99728928+221RDB161@users.noreply.github.com>
2023-03-05 00:18:24 +0000

    221RDB161(test7)

--- tree_height.py
+++ tree_height.py
@@ -40,16 +40,20 @@ def read_input_from_file(file_path):
         with open(file_path, "r") as f:
             text = f.read().strip()
         return text.split('\n')
+
+    except FileNotFoundError:
+        print("Neatbilstošs faila path")
+        return None
     
     except:
         print("Kļūda nolasot failu")
         return None
 
 def main():
-    input_veids = input("Ievadi 'F' lai nolasītu inputu no faila, vai arī 'I' lai nolasītu input no klaviatūras: ").strip()
+    input_veids = input("").strip()
     
     if input_veids == 'F':
-        file_name = input("Ievadi faila nosaukumu. (Tie faila nosaukumi kuros būs burts 'a' nedarbosies: ")
+        file_name = input("")
         
         if 'a' in file_name:
             print("Neatbilstošs faila nosaukums")
